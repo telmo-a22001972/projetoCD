5559405cebe24dd65fd720d03f47a778f1e2663f
Telmo Panaças <telmoalexandre2505@gmail.com>
2022-11-04 17:03:37 +0000

    Changes by isa

--- src/ServerImpl.java
+++ src/ServerImpl.java
@@ -1,6 +1,9 @@
 import java.rmi.*;
 import java.rmi.server.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.util.ArrayList;
+import java.sql.*;
 
 public class ServerImpl extends UnicastRemoteObject
   implements ServerIntf {
@@ -8,10 +11,47 @@ public class ServerImpl extends UnicastRemoteObject
   public ServerImpl() throws RemoteException {
   }
 
-  public boolean reservarMesa(String idMesa) throws RemoteException {
-    ReadFiles.readMesas();
+  public boolean reservarMesa(String idMesa, String data, String horario) throws RemoteException {
+    Credentials cred = new Credentials();
+    String result = "";
+    System.out.println("Pedido reservar mesas recebido\n");
 
-    return true;
+    try{
+
+      Connection con = DriverManager.getConnection(cred.getUrl(), cred.getUser(), cred.getPassword());
+
+      PreparedStatement query = con.prepareStatement("SELECT * FROM reserva WHERE mesa_id = ? AND horario = ? AND dia = ?");
+      query.setInt(1, Integer.parseInt(idMesa));
+      query.setString(2, horario);
+      query.setDate(3, Date.valueOf(data));
+
+      Statement st = con.createStatement();
+      ResultSet rs = query.executeQuery();
+
+      if(rs.next()){
+        //existe
+        System.out.println("Registo Encontrado\n");
+        System.out.println("Resposta ao pedido reservar mesa efetuada");
+
+        //Já existe reserva
+        return false;
+      }else {
+        System.out.println("Nao existe registo\n");
+        System.out.println("Resposta ao pedido reservar mesa efetuada");
+
+        String s = "insert into reserva(dia, horario, pessoa_id, mesa_id) values(" + "\"" + data+ "\"" + "," + "\""+ horario + "\"" + ", 1," + idMesa + ");";
+
+        st.execute(s);
+
+
+        //Não existe reserva
+        return true;
+      }
+
+    } catch(Exception e){
+      System.out.println(e);
+      return false;
+    }
   }
 
   public boolean cancelarMesa(String idMesa, String data, String horario) throws RemoteException {
@@ -22,7 +62,5 @@ public class ServerImpl extends UnicastRemoteObject
     return new ArrayList<>();
   }
 
-  public boolean registarUtilizador(String username, String password) throws RemoteException {
-    return true;
-  }
+
 }
--- src/ServerIntf.java
+++ src/ServerIntf.java
@@ -2,7 +2,7 @@ import java.rmi.*;
 import java.util.ArrayList;
 
 public interface ServerIntf extends Remote {
-  boolean reservarMesa(String idMesa) throws RemoteException;
+  boolean reservarMesa(String idMesa, String data, String horario) throws RemoteException;
   boolean cancelarMesa(String idMesa, String data, String horario) throws RemoteException;
   ArrayList<String> listarMesas() throws RemoteException;
   boolean registarUtilizador(String username, String password) throws RemoteException;
